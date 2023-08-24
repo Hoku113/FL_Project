6cdb789ee7fcddea40e6549f18f4ecaaf9fe7dc5
Hoku <hokuto.u0113@gmail.com>
2023-08-25 00:57:50 +0900

    preprocess

--- OwnerFunc/run.ps1
+++ OwnerFunc/run.ps1
@@ -7,15 +7,29 @@ param($Request, $TriggerMetadata)
 Write-Host "PowerShell HTTP trigger function processed a request."
 
 # Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
+$container_name = $Request.Query.Name
+
+if (-not $container_name) {
+    $container_name = $Request.Body.Container
 }
+
 
 $body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
 
-if ($name) {
-    $body = "Hello, $name. This HTTP triggered function executed successfully."
+# Create Container Object
+$port1 = New-AzContainerInstancePortObject -Port 8000
+# $port2 = New-AzContainerInstancePortObject -Port 8001
+
+# Debug
+Write-Output $port1
+# Write-Output $port2
+
+# $container = New-AzContainerInstanceObject -Name $container_name -Image nginx -RequestCpu 2 -RequestMemoryInGb 4 -Port @($port1, $port2)
+# $containerGroup = New-AzContainerGroup -ResourceGroupName Federated_Learning_tutorial -Name $container_name -Location eastus -Container $container -OSType 'Linux' -RestartPolicy "Never" -IPAddressType 'Public'
+
+
+if ($container_name) {
+    $body = "Hello, $container_name. This HTTP triggered function executed successfully."
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
