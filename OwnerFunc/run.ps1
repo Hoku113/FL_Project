2eb71d237a07b56dd93878459014d33fb4c8e559
Hoku113 <hokuto.u0113@gmail.com>
2023-08-29 02:03:49 +0900

    create container instance

--- OwnerFunc/run.ps1
+++ OwnerFunc/run.ps1
@@ -17,16 +17,18 @@ if (-not $container_name) {
 $body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
 
 # Create Container Object
+
+Write-Host "--------------- Creating your Director node -------------------------"
+
 $port1 = New-AzContainerInstancePortObject -Port 8000
-# $port2 = New-AzContainerInstancePortObject -Port 8001
+$port2 = New-AzContainerInstancePortObject -Port 8001
 
 # Debug
-Write-Output $port1
+# Write-Output $port1
 # Write-Output $port2
 
-# $container = New-AzContainerInstanceObject -Name $container_name -Image nginx -RequestCpu 2 -RequestMemoryInGb 4 -Port @($port1, $port2)
-# $containerGroup = New-AzContainerGroup -ResourceGroupName Federated_Learning_tutorial -Name $container_name -Location eastus -Container $container -OSType 'Linux' -RestartPolicy "Never" -IPAddressType 'Public'
-
+$container = New-AzContainerInstanceObject -Name $container_name -Image nginx -Port @($port1, $port2)
+New-AzContainerGroup -ResourceGroupName Federated_Learning_tutorial -Name "director-resource-creator" -Location japaneast -Container $container -OSType 'Linux' -RestartPolicy "OnFailure" -IPAddressType 'Public'
 
 if ($container_name) {
     $body = "Hello, $container_name. This HTTP triggered function executed successfully."
